0165a704303c673f741f79f76d7f9b20492a7671
Leo Rosales <leorosal@usc.edu>
2023-12-24 13:12:10 -0800

    Added basic inputs to app page

--- pickleball/src/Page/Page.js
+++ pickleball/src/Page/Page.js
@@ -1,8 +1,45 @@
 import React from "react";
 
 const Page = () => {
+    const HandleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div>Leo and Linda's new pickleball app!</div>
+        <div>
+            <h1>Leo and Linda's new pickleball app!</h1>
+            <form onSubmit={HandleSubmit}>
+                <div>
+                    <label>
+                        Number of Players:
+                        <input type="number" />
+                    </label>
+                </div>
+
+                <div>
+                    <label>
+                        Number of Courts:
+                        <input type="number" />
+                    </label>
+                </div>
+
+                <div>
+                    <label>
+                        Games per Player:
+                        <input type="number" />
+                    </label>
+                </div>
+
+                <button type="Submit">Generate Schedule</button>
+
+            </form>
+
+            <div>
+                <h2>Schedule: </h2>
+
+            </div>
+        </div>
+
     )
 };
 
